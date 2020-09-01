a7417c6e0a409c3e00b827d6668dc4d57d512113
marobew <boramwe.dev@gmail.com>
2020-09-02 00:14:54 +0900

    Creating the Apps

--- config/settings.py
+++ config/settings.py
@@ -52,7 +52,12 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "user",
+    "conversations",
+    "lists",
+    "reservations",
+    "reviews",
+    "rooms",
+    "users",
 ]
 
 MIDDLEWARE = [
